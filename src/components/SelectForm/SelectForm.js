d7f6b990790cdf5b94588c51c502dc824faf6d10
Ranacode <romerodeveloper@techie.com>
2018-03-09 19:44:45 +0000

    forms effects

--- src/components/SelectForm/SelectForm.js
+++ src/components/SelectForm/SelectForm.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
 import defaultImage from '../../assets/images/selectForms/defaultForm.jpg';
+import './SelectForm.css';
 
 class SelectForm extends Component {
   state = {};
   render() {
     return (
-      <Grid style={{ marginTop: '3em' }}>
+      <Grid className="selectGrid">
         <Row>
           <Col md={4}>
             <Image alt="defaultImage" src={defaultImage} thumbnail responsive />
