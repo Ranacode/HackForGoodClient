f4c47e4ac7ecf82765fcebfe6a226310a8b09365
Iru Hernández <iru343@hotmail.com>
2018-03-09 14:58:10 +0100

    home and activities

--- src/components/Avtivities/Activities.js
+++ src/components/Avtivities/Activities.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import { Grid, Row, Col } from 'react-bootstrap';
+
 class Activities extends Component {
-    state = {}
-    render() { 
-        return (  )
-    }
+  state = {};
+  render() {
+    return <div />;
+  }
 }
- 
+
 export default Activities;
