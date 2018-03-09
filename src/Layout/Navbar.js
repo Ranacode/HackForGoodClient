a1708e80da91393103b97a14b19d40c163de5682
Ranacode <romerodeveloper@techie.com>
2018-03-09 10:02:44 +0000

    added bootstrap

---
+++ src/Layout/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Grid, Row, Navbar as Nav } from 'react-bootstrap';
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return <div>navbar</div>;
+  }
+}
+
+export default Navbar;
